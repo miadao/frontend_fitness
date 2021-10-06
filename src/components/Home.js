e2bb2033c8526cdfe55fd903f5513b2382cb6db2
Michael Simo <michaelsimo@att.net>
2021-10-05 21:27:28 -0400

    starting home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import { BASE_URL } from "../api";
 
 const Home = ({}) => {
-
-
+    return <div id="title">
+    <h1>
+       Fitness Tracker
+     </h1>
+   <ul className="header">
+   <li><a href="/">Home</a></li>
+   <NavLink to="/login">Login</NavLink>
+   <NavLink to="/register">Register</NavLink>
+   <NavLink to="/routines">Routines</NavLink>
+ </ul>
+ </div>
 }
+
 export default Home;
