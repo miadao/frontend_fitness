69987ea7a18d0cc7009d6b25de4b066015aba07e
Sean <seanc878@yahoo.com>
2021-10-07 23:45:25 -0600

    activities nearly complete

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -10,8 +10,11 @@ const Activities = () => {
     const [editedname, setEditedName] = useState('')
     const [editeddescription, setEditedDescription] = useState('')
     const [isEditing, setIsEditing] = useState(false)
+    const [requestedRoutines, setRequestedRoutines] = useState(false)
+    const [routinesToDisplay, setRoutinesToDisplay] = useState([])
     const [idToEdit, setIdToEdit] = useState('')
     const [nameToEdit, setNameToEdit] = useState('')
+  
     
 
     useEffect(() => {
@@ -23,6 +26,7 @@ const Activities = () => {
         }
         fetchActivities()  
     }, [])
+
 
     function newActivity(name, description){
         fetch(`${BASE_URL}/activities`, {
@@ -58,48 +62,108 @@ const Activities = () => {
             })
     }
 
+    function getRoutinesByActivity(activityId){
+        
+        fetch(`${BASE_URL}/activities/${activityId}/routines`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(response => response.json())
+            .then(result => { 
+                setRoutinesToDisplay(result)
+                console.log(result)
+            }).
+            catch(console.error)
+    }
+
+
     return ( <>
      { isEditing ? 
-        <><div className="editactivityform">
-                        <h1>Edit Activity Form</h1>
-                        <h2>Editing Activity: {nameToEdit}</h2>
+            <>
+                <div className="editactivityform">
+                            <h1>Edit Activity Form</h1>
+                            <h2>Editing Activity: {nameToEdit}</h2>
+                            <label>Activity Name</label>
+                            <input className="editactivityname" type="text" value={editedname} onChange={event => { setEditedName(event.target.value)}}></input>
+                            <label>Activity Description</label>
+                            <input className="editactivitydescription" type="text" value={editeddescription} onChange={event => { setEditedDescription(event.target.value)}}></input>
+                            <button className="editactivitybutton" onClick={function(){editActivity(editedname, editeddescription), setIsEditing(false)}}>Edit Activity!</button>
+                            <button className="cancelbutton" onClick={function (){setIsEditing(false)}}>Cancel</button>
+                            </div> 
+            </>
+        :
+       
+            <>
+                
+                <h1>Activities</h1>
+                <div className="activitycontainer">
+                    <div className="mapcontainer">
+                        {activities.map(activity => {
+                            return (
+                                <div className="activity" key={activity.id}>
+                                    <div>Name: {activity.name}</div>
+                                    <div className="activityDescription">Description: {activity.description}</div>
+                                    <button className="editButton"  disabled={requestedRoutines || !token} onClick={function(){
+                                        setIsEditing(true)
+                                        setIdToEdit(activity.id)
+                                        setNameToEdit(activity.name)
+                                        console.log(activity.id)
+                                        }}>Edit</button>
+                                        
+                                    <button className="seeroutines" disabled={requestedRoutines} onClick={(event) => { 
+                                        event.preventDefault();
+                                        getRoutinesByActivity(activity.id) 
+                                        setRequestedRoutines(true);
+                                        
+                                        console.log(activity.id)
+                                        }}>See Routines!</button>
+                                </div> 
+                            
+                            )  
+                        })}
+                    </div>
+                    <div className="newactivityform">
+                        <h1>New Activity Form</h1>
                         <label>Activity Name</label>
-                        <input className="editactivityname" type="text" value={editedname} onChange={event => { setEditedName(event.target.value)}}></input>
+                        <input className="newactivityname" type="text" value={name} onChange={event => { setName(event.target.value)}}></input>
                         <label>Activity Description</label>
-                        <input className="editactivitydescription" type="text" value={editeddescription} onChange={event => { setEditedDescription(event.target.value)}}></input>
-                        <button className="editactivitybutton" onClick={function(){editActivity(editedname, editeddescription), setIsEditing(false)}}>Edit Activity!</button>
-                        <button className="cancelbutton" onClick={function (){setIsEditing(false)}}>Cancel</button>
-                    </div> </>
-        :  
-        <> 
-            <h1>Activities</h1>
-            {activities.map(activity => {
-                return (
-                <div className="activity" key={activity.id}>
-                    <div>Name: {activity.name}</div>
-                    <div className="activityDescription">Description: {activity.description}</div>
-                    <button className="editButton" onClick={function(){
-                        setIsEditing(true)
-                        setIdToEdit(activity.id)
-                        setNameToEdit(activity.name)
-                        console.log(idToEdit)
-                        }}>Edit</button>
-                </div> 
-                )
-                
-                
-            })}
-            <div className="newactivityform">
-                    <h1>New Activity Form</h1>
-                    <label>Activity Name</label>
-                    <input className="newactivityname" type="text" value={name} onChange={event => { setName(event.target.value)}}></input>
-                    <label>Activity Description</label>
-                    <input className="newactivitydescription" type="text" value={description} onChange={event => { setDescription(event.target.value)}}></input>
-                    <button className="newactivitybutton" onClick={function(){newActivity(name, description)}}>Add Activity!</button>
-            </div>
-        </>
-          }</>)
-        
-
+                        <input className="newactivitydescription" type="text" value={description} onChange={event => { setDescription(event.target.value)}}></input>
+                        <button className="newactivitybutton" disabled={!token} onClick={function(){newActivity(name, description)}}>Add Activity!</button>
+                    </div>
+                    
+                    { requestedRoutines ?
+                        <> 
+                            <div className="routinemapcontainer">         
+                                <h1>Routines</h1>    
+                                <button className="closebutton" onClick={(event) => {
+                                    event.preventDefault()
+                                    setRequestedRoutines(false)
+                                    }}>Close</button>
+                            
+                                {routinesToDisplay.map(routine => {
+                                    return (
+                                        <div className="routine" key={routine.id}>
+                                            <div>Created By: {routine.creatorName}</div>
+                                            <div>Routine Name: {routine.name} </div>
+                                            <div>Activities: 
+                                                {routine.activities.map(activity => {
+                                                    return (<div key={activity.id}>
+                                                                <div>{activity.name}</div>
+                                                                <div>{activity.description}</div>
+                                                            </div>) 
+                                                })}
+                                            </div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </>
+                    : null      
+                    }
+                </div>
+            </>          
     }
+          </> )        
+}
+
 export default Activities;
